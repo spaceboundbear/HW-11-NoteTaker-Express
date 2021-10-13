4175c6d82de85b47458a12c658e9028c8c7d7500
spaceboundbear <mf.michaelfischer@gmail.com>
2021-10-13 01:28:47 -0500

    code working, pushing to heroku

--- helpers/uuid.js
+++
@@ -1,5 +0,0 @@
-const { v4: uuidv4 } = require('uuid');
-
-let uuid = uuidv4();
-
-module.exports = uuid;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
-const api = require('./routes/apiroutes');
-const html = require('./routes/htmlroutes');
+const path = require('path');
+const fs = require('fs');
+const db = require('./db/db');
+const { v4: uuidv4 } = require('uuid');
+let uuid = uuidv4();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,8 +12,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api', api);
-app.use('/', html);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app
+  .route('/api/notes')
+  .get((req, res) => {
+    res.json(db);
+  })
+  .post((req, res) => {
+    const dbFilePath = path.join(__dirname, '/db/db.json');
+    const notes = req.body;
+    notes.id = uuidv4();
+    db.push(notes);
+    fs.writeFile(dbFilePath, JSON.stringify(db), (err) => {
+      if (err) {
+        return err;
+      }
+    });
+    res.json(notes);
+  });
+
+app.delete('/api/notes/:id', function (req, res) {
+  fs.readFile('./db/db.json', 'utf8', function (err, data) {
+    const db = JSON.parse(data);
+    fs.writeFile(
+      './db/db.json',
+      JSON.stringify(db.filter((note) => note.id !== req.params.id)),
+      () => res.send('Success!')
+    );
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
